ae63978d72aad964c16698d1dd492b5e4d147409
Aditya Mishra <adityamishra2348@gmail.com>
2025-11-27 15:50:43 +0530

    Remove .next from git tracking

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"WrXB4wC7CICtwUBdAfIM0v7uPohQGlhXXOdUO77velo=\"\n}"
--- .next/static/chunks/src_app_layout_tsx_c0237562._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_app_globals_b805903d.css",
-  "static/chunks/node_modules_3f0e3fcc._.js",
-  "static/chunks/src_32734195._.js"
-],
-    source: "dynamic"
-});
